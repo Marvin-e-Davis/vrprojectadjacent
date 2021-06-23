c4dcbd6327440b823671fb11884cb6c242fe22cd
em-beth <85454614+em-beth@users.noreply.github.com>
2021-06-23 16:15:30 -0500

    Saved all youtube ids for each show

---
+++ results.js
@@ -0,0 +1,10 @@
+//data structure to hold video urls
+
+var urls = new Map([
+  ['Haikyuu', 'XS-N8KfZ5EU'],
+  ['Gurren Lagann', 'FwgMxjhXkKo'],
+  ['FMAB', '2uq34TeWEdQ'],
+  ['Death Note','8QE9cmfxx4s'],
+  ['HxH', '8QE9cmfxx4s'],
+  ['Steins Gate', '8QE9cmfxx4s']
+]);
